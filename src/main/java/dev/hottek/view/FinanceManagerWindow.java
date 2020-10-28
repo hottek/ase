eea84a45dfaac5bcb1af01bdf3c0e7cb4d5f7030
hottek <dev@mardybum.de>
2020-10-29 00:33:26 +0100

    Add basic menu bar to UI

--- src/main/java/dev/hottek/view/FinanceManagerWindow.java
+++ src/main/java/dev/hottek/view/FinanceManagerWindow.java
@@ -11,6 +11,12 @@ public class FinanceManagerWindow extends JFrame {
     private void initializeUI() {
         this.setTitle("Finance Manager");
         this.setSize(600,600);
+        JMenuBar menuBar = new JMenuBar();
+        JMenu options = new JMenu("Options");
+        JMenuItem oSave = new JMenuItem("Save");
+        options.add(oSave);
+        menuBar.add(options);
+        this.setJMenuBar(menuBar);
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
