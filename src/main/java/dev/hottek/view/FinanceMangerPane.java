89a95c57c004ce81dfa6358ccd5690bef20f97a1
hottek <dev@mardybum.de>
2021-02-06 11:40:48 +0100

    Refactor FinanceManagerPane

--- src/main/java/dev/hottek/view/FinanceMangerPane.java
+++ src/main/java/dev/hottek/view/FinanceMangerPane.java
@@ -5,18 +5,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FinanceMangerPane extends JTabbedPane {
-    public Map<String, AccountPanel> accountPanelMap;
+    public Map<String, JPanel> accountPanelMap;
     public FinanceMangerPane() {
         this.setBounds(50,50,200,200);
         this.accountPanelMap = new HashMap<>();
-        addAccountPanel("Overview");
+        addOverviewPanel();
         addAccountPanel("Account1");
         addAccountPanel("Account2");
     }
 
-    public void addAccountPanel(String accountName) {
-        AccountPanel accountPanel = new AccountPanel(accountName);
-        this.accountPanelMap.put(accountName, accountPanel);
-        this.add(accountName, accountPanel);
+    public void addOverviewPanel() {
+        String panelName = "Overview";
+        OverviewPanel overviewPanel = new OverviewPanel();
+        this.accountPanelMap.put(panelName, overviewPanel);
+        this.add(panelName, overviewPanel);
+    }
+
+    public void addAccountPanel(String panelName) {
+        AccountPanel accountPanel = new AccountPanel(panelName);
+        this.accountPanelMap.put(panelName, accountPanel);
+        this.add(panelName, accountPanel);
     }
 }
