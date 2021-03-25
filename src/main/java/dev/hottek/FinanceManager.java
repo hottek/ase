b909974e91b8aaeb679f0bb402c79304173de7a9
hottek <dev@mardybum.de>
2021-03-25 20:44:46 +0100

    Add material LookAndFeel
    Not sure if this theme will be kept for the final version

--- src/main/java/dev/hottek/FinanceManager.java
+++ src/main/java/dev/hottek/FinanceManager.java
@@ -1,11 +1,16 @@
 package dev.hottek;
 
+import mdlaf.MaterialLookAndFeel;
+import mdlaf.themes.MaterialLiteTheme;
+import mdlaf.themes.MaterialOceanicTheme;
+
 import javax.swing.*;
 
 public class FinanceManager {
     public static void main(String[] args) {
         try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            // UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.setLookAndFeel(new MaterialLookAndFeel(new MaterialLiteTheme())); // https://github.com/vincenzopalazzo/material-ui-swing
         } catch (Exception e) {
             e.printStackTrace();
         }
